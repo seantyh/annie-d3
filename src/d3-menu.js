8257d7b444bdc67ad1b167e269f39822c131cb4c
seantyh <seantyh@gmail.com>
2019-02-27 22:58:00 +0800

    refactor to individual js and connect scss

---
+++ src/d3-menu.js
@@ -0,0 +1,19 @@
+import * as d3 from "d3";
+
+export let init_menu = function(){
+  const svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+  let radius = 10;
+
+  let c1 = svg.append("circle")
+    .attr("r", radius)
+    .attr("class", "fixedCircle")
+    .attr("cx", width-radius*2)
+    .attr("cy", height-radius*2);
+  
+    c1.on("click", ()=>{
+      console.log("I am clicked");
+    })
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 var path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", "css-loader", "sass-loader"
+      ]
+    }]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
